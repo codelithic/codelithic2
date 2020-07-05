50bd357fb9c195b8ae40f6ab2dbd7c519f4ef0c5
codelithic <roberto.dirienzo@codelithic.com>
2020-07-05 14:33:53 +0200

    deps updated

--- 3ty/codelithic/include/codelithic/web/utils/query.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef BEEBLEBROX_PERSISTENCE_QUERY_ROUTE_HPP
-#define BEEBLEBROX_PERSISTENCE_QUERY_ROUTE_HPP
-
-#define QUERY_LOGIN ("select id from users where name=$1 and password=$2")
-#define QUERY_SERVER_CONFIG ("select * from beeblebrox")
-
-#endif
